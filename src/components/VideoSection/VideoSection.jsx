5c16ab5d3a52c42f256dbe24f331ad8348d3d858
Kauã Gomes <kauaoficialkr@outlook.com>
2024-11-19 03:43:17 -0300

    add loading lazy no iframe

--- src/components/VideoSection/VideoSection.jsx
+++ src/components/VideoSection/VideoSection.jsx
@@ -4,7 +4,7 @@ export function VideoSection() {
     return (
         <div className={styles.video} id='video'>
             <span className={styles.phrase}>CHECK IT OUT</span>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/TzfnlPxCZv0?si=6ibilPOSoJZXuswa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/TzfnlPxCZv0?si=6ibilPOSoJZXuswa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
     )
 }

8c9a4788dcaf67d1cde34884f69f71df76d4e62b
Kauã Gomes <kauaoficialkr@outlook.com>
2024-11-21 21:18:43 -0300

    adicionado video

--- src/components/VideoSection/VideoSection.jsx
+++ src/components/VideoSection/VideoSection.jsx
@@ -7,7 +7,7 @@ export function VideoSection() {
             <iframe 
                 width="560" 
                 height="315" 
-                src="https://www.youtube.com/embed/4vzYzhQqU5w?si=Ft8Ck6p-tnPLNT1W" 
+                src="https://youtu.be/djAW-9vO2_0?si=SaRNXDKtkhKT-XXk" 
                 title="YouTube video player" 
                 frameBorder="0"  
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
